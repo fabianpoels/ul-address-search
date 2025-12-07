fed8f8c390bc52bfc17ea5e47ccbdeaa6e916438
Fabian Poels <fabian@fabianpoels.com>
2025-12-07 23:01:11 +0100

    use config url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import { type Address } from '../../../shared/types/address'
+import { config } from '../config/config'
 
 async function search(query: string, signal?: AbortSignal): Promise<Array<Address>> {
-  const response = await fetch(`http://localhost:8080/api/v1/search/${query}`, {
+  const response = await fetch(`${config.apiUrl}/search/${query}`, {
     signal,
   })
   const result = (await response.json()) as Array<Address>
