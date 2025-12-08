345878b7dd1d13059debf67d54b41b872d75e062
Fabian Poels <fabian@fabianpoels.com>
2025-12-08 12:00:15 +0100

    clean api address response

--- api/src/controllers/search.controller.ts
+++ api/src/controllers/search.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { addressTrie } from '../data/addressTrie'
+import { type Address } from '@ul-address-search/shared'
 import { config } from '../config/applicationConfig'
 
 const search = async (req: Request, res: Response, next: NextFunction) => {
@@ -12,7 +13,18 @@ const search = async (req: Request, res: Response, next: NextFunction) => {
 
   // TODO: cleaner handling of potential Trie errors?
   try {
-    return res.send(addressTrie.search(req.params.query).slice(0, config.maxSearchResults))
+    // trie-search adds the '$tsid' property on the addresses to make sure every entry has a unique ID
+    // we allow this, as it is probably more performant and robust than coming up with our own unique ID solution
+    // to keep the response consistent with the Address type (and the assignment),
+    // we make sure to extract only the Address fields from the search result
+    const result = addressTrie
+      .search(req.params.query)
+      .slice(0, config.maxSearchResults)
+      .map((result) => {
+        const { $tsid, ...address } = result as any
+        return address as Address
+      })
+    return res.send(result)
   } catch (e) {
     next(e)
   }
